d005dd6fc5d952f43804b1c602b03606c9120e42
keisatou <dummy>
2017-04-09 19:33:22 +0900

    add Quotes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
   <div class="container">
-
+    <app-quote-grid :quotes="quotes" :name="name"></app-quote-grid>
   </div>
 </template>
 
 <script>
-export default {
+import QuoteGrid from './components/QuoteGrid.vue';
 
+export default {
+  data() {
+    return {
+      name: 'app-quote-grid',
+      quotes: [
+        'Just a Quote to see something',
+      ],
+      maxQuotes: 10,
+    };
+  },
+  components: {
+    'app-quote-grid': QuoteGrid,
+  },
 };
 </script>
 
