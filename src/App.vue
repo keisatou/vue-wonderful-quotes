ea661319c5ddd12e26c69355a9d2e1efc355a567
keisatou <dummy>
2017-04-11 22:06:29 +0900

    add button to create Quote

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="container">
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
     <app-quote-grid :quotes="quotes" :name="name"></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import QuoteGrid from './components/QuoteGrid.vue';
+import NewQuote from './components/NewQuote.vue';
 
 export default {
   data() {
@@ -17,8 +23,14 @@ export default {
       maxQuotes: 10,
     };
   },
+  methods: {
+    newQuote(quote) {
+      this.quotes.push(quote);
+    },
+  },
   components: {
     'app-quote-grid': QuoteGrid,
+    'app-new-quote': NewQuote,
   },
 };
 </script>
